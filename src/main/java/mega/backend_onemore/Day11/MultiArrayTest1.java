5287eb706f7e42c18e3cb972931de820b150272c
EUIJOON <euijoonism@gmail.com>
2023-04-15 17:46:23 +0900

    [Day11] 예제 추가

--- src/main/java/mega/backend_onemore/Day11/MultiArrayTest1.java
+++ src/main/java/mega/backend_onemore/Day11/MultiArrayTest1.java
@@ -4,23 +4,26 @@ public class MultiArrayTest1 {
 
   public static void main(String[] args) {
 
+    // 2차원 배열 d를 생성하고, 각 차원에 맞게 크기를 지정한다.
     int[][] d = new int[3][];
     d[0] = new int[5];
     d[1] = new int[2];
     d[2] = new int[3];
 
-    for(int i = 0;i<d.length;i++) {
-      for(int j=0;j<d[i].length;j++) {
+    // 2차원 배열 d의 값을 9로 초기화하고 출력한다.
+    for (int i = 0; i < d.length; i++) {
+      for (int j = 0; j < d[i].length; j++) {
         d[i][j] = 9;
-        System.out.print(d[i][j]+"\t");
+        System.out.print(d[i][j] + "\t");
       }
       System.out.println();
     }
 
-    int [][][] k = new int[5][][];
+    // 3차원 배열 k를 생성하고, 첫 번째 차원에 맞게 크기를 지정한다.
+    int[][][] k = new int[5][][];
 
+    // k의 첫 번째 차원의 첫 번째 원소에 대해, 두 번째와 세 번째 차원에 맞게 크기를 지정한다.
     k[0] = new int[5][3];
 
   }
-
 }
--- src/main/java/mega/backend_onemore/Day11/MultiArrayTest2.java
+++ src/main/java/mega/backend_onemore/Day11/MultiArrayTest2.java
@@ -1,12 +1,15 @@
 package mega.backend_onemore.Day11;
 
 public class MultiArrayTest2 {
+
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    // 3차원 배열 b를 생성하고, 각 차원에 맞게 크기를 지정한다.
     int[][][] b= new int[2][2][2];
 
+    // count 변수를 이용하여 배열의 값을 1부터 순서대로 증가시킨다.
     int count = 1;
 
+    // 이중 for문과 삼중 for문을 이용하여 배열의 값을 순서대로 할당한다.
     for(int i = 0;i<b.length;i++) {
       for(int j = 0;j<b[i].length;j++) {
         for(int x = 0;x<b[i][j].length;x++) {
@@ -14,6 +17,8 @@ public class MultiArrayTest2 {
         }
       }
     }
+
+    // 이중 for문과 삼중 for문을 이용하여 배열의 값을 출력한다.
     for(int i = 0;i<b.length;i++) {
       for(int j = 0;j<b[i].length;j++) {
         for(int x = 0;x<b[i][j].length;x++) {
@@ -23,7 +28,6 @@ public class MultiArrayTest2 {
       }
       System.out.println();
     }
-
   }
 
 }
