37074af9a462e91d15540f79c055c57293425fd6
EUIJOON <euijoonism@gmail.com>
2023-04-15 14:47:36 +0900

    First Commit

---
+++ src/main/java/mega/backend_onemore/BackendOnemoreApplication.java
@@ -0,0 +1,13 @@
+package mega.backend_onemore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnemoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOnemoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mega/backend_onemore/BackendOnemoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mega.backend_onemore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOnemoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
