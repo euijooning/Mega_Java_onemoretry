35685379bf7fd816448b4a9c86997375e9ca810e
EUIJOON <euijoonism@gmail.com>
2023-05-13 03:09:08 +0900

    [인프런] 객체배열 필기 후

--- src/main/java/mega/backend_onemore/_inf/OOP/ObjectArray/ObjArray01.java
+++ src/main/java/mega/backend_onemore/_inf/OOP/ObjectArray/ObjArray01.java
@@ -1,5 +1,7 @@
 package mega.backend_onemore._inf.OOP.ObjectArray;
-
+// <문제>
+// 객체에 대한 참조값을 요소로 가지는 객체 배열을 생성하는 코드를 구현하시오.
+// 반복문을 사용하여 객체를 생성하시오.
 class Person01 {
   // Field
   private String name;
@@ -38,19 +40,19 @@ public class ObjArray01 {
     // [1] : 객체 생성
     int[] ar1 = {1, 2, 3, 4, 5};
     char[] ar2 = {'A', 'B', 'C', 'D', 'E'};
-    Person01[] pa = new Person01[5];
-    pa[0] = new Person01("홍길동", 20);
-    pa[1] = new Person01("박길동", 21);
-    pa[2] = new Person01("김길동", 22);
-    pa[3] = new Person01("장길동", 23);
-    pa[4] = new Person01("고길동", 24);
+    Person01[] ps = new Person01[5];
+    ps[0] = new Person01("홍길동", 20);
+    ps[1] = new Person01("박길동", 21);
+    ps[2] = new Person01("김길동", 22);
+    ps[3] = new Person01("장길동", 23);
+    ps[4] = new Person01("고길동", 24);
 
     // [2] : 출력
-    for (int i = 0; i < pa.length; i++) {
-      // System.out.print( pa[i].name + "\t\t" );
-      System.out.print(pa[i].getAge() + "\t\t");
+    for (int i = 0; i < ps.length; i++) {
+      // System.out.print( pa[i].name + "\t\t" );//private이므로 안 됨.
+      System.out.println(ps[i].getName());
     }
     System.out.println();
-  }
 
+  }
 }
--- src/main/java/mega/backend_onemore/_inf/OOP/ObjectArray/ObjArray02.java
+++ src/main/java/mega/backend_onemore/_inf/OOP/ObjectArray/ObjArray02.java
@@ -3,7 +3,6 @@ package mega.backend_onemore._inf.OOP.ObjectArray;
 // 반복문(for)을 사용하여 객체 배열을 생성하고 출력해보시오.
 // 이 문제는 객체의 주소를 저정하고 있는 객체 배열을 반복문을 사용하여 만들 수 있는지를 묻는 문제이다.
 
-
 class Person02 {
   // Field
   private String name;
@@ -41,15 +40,15 @@ public class ObjArray02 {
   public static void main(String[] args) {
 
     // [1] : 객체 생성
-    Person02[] pa;				// Person 타입의 객체 배열 변수 pa 선언.
-    pa = new Person02[5];		// Person 객체의 참조값을 원소로 가지는 배열 공간을 5개 생성 --> 5명 객체 생성할테니 주소 저장해라.
+    Person02[] p;				// Person 타입의 객체 배열 변수 pa 선언.
+    p = new Person02[5];		// Person 객체의 참조값을 원소로 가지는 배열 공간을 5개 생성 --> 5명 객체 생성할테니 주소 저장해라.
 
     // [2] : 반복문(for)을 돌면서 Person() 객체 생성
-    for( int i=0; i < pa.length; i++ ) {
-      pa[i] = new Person02( i+"번 후보자", i+20 );  // i만 입력하면 안되나 문자열을 붙이면 문자열로 전달.
+    for( int i=0; i < p.length; i++ ) {
+      p[i] = new Person02( i+"번 후보자", i+20 );  // i만 입력하면 안되나 문자열을 붙이면 문자열로 전달.
 
       // [2-1] : getter, setter 메서드 사용해서 출력
-      System.out.println( pa[i].getName() + " --> 나이 : " + pa[i].getAge() );
+      System.out.println( p[i].getName() + " --> 나이 : " + p[i].getAge() );
 
       // [2-2] : 배열 인덱스 사용해서 출력 --> 단, 이렇게 쓰려면 private 선언을 삭제하고 써야함.
       // System.out.println( pa[i].name + "의 나이는 " + pa[i].age + "살 입니다." );
